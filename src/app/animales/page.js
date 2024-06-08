f4ecab368d441ced44bbb61fe826f71580b985be
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-06-08 17:57:12 +0200

    add skeletons

--- src/app/animales/page.js
+++ src/app/animales/page.js
@@ -1,38 +1,25 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Animal from '@/components/Animal'
-import { getAnimales } from '@/lib/actions'
+import Animales from '@/components/Animales'
+import { Suspense } from 'react'
 
-export const dynamic = 'force-dynamic'
 
 export default async function Home() {
-    const animales = await getAnimales()
-    // console.log(animales);
-
     return (
         <div>
             <Link className='enlace' href="/animales/new">
                 <Image src='/nuevo.svg' alt='nuevo' width="20" height="20" />
                 Nuevo animal
             </Link>
-            {
-                animales.map((animal) => (
-                    <Animal key={animal.id} animal={animal} >
-                        <Link className='enlace' href={`/animales/view/${animal.id}`} >
-                            <Image src='/ver.svg' alt='nuevo' width="20" height="20" />
-                            Ver
-                        </Link>
-                        <Link className='enlace' href={`/animales/edit/${animal.id}`} >
-                            <Image src='/editar.svg' alt='nuevo' width="20" height="20" />
-                            Editar
-                        </Link>
-                        <Link className='enlace' href={`/animales/delete/${animal.id}`} >
-                            <Image src='/eliminar.svg' alt='nuevo' width="20" height="20" />
-                            Eliminar
-                        </Link>
-                    </Animal>
-                ))
-            }
+            <Suspense fallback={
+                <>
+                    <div className='esqueleto-animal' />
+                    <div className='esqueleto-animal' />
+                    <div className='esqueleto-animal' />
+                </>
+            }>
+                <Animales />
+            </Suspense>           
         </div>
     )
 }
--- src/app/zoos/page.js
+++ src/app/zoos/page.js
@@ -1,38 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import Zoo from '@/components/Zoo'
-import { getZoos } from '@/lib/actions'
+import Zoos from '@/components/Zoos'
+import { Suspense } from 'react'
 
-export const dynamic = 'force-dynamic'
 
 export default async function Home() {
-    const zoos = await getZoos()
-    // console.log(zoos);
-
     return (
         <div>
             <Link className='enlace' href="/zoos/new">
                 <Image src='/nuevo.svg' alt='nuevo' width="20" height="20" />
                 Nuevo zoológico
             </Link>
-            {
-                zoos.map((zoo) => (
-                    <Zoo key={zoo.id} zoo={zoo} >
-                        <Link className='enlace' href={`/zoos/view/${zoo.id}`} >
-                            <Image src='/ver.svg' alt='nuevo' width="20" height="20" />
-                            Ver
-                        </Link>
-                        <Link className='enlace' href={`/zoos/edit/${zoo.id}`} >
-                            <Image src='/editar.svg' alt='nuevo' width="20" height="20" />
-                            Editar
-                        </Link>
-                        <Link className='enlace' href={`/zoos/delete/${zoo.id}`} >
-                            <Image src='/eliminar.svg' alt='nuevo' width="20" height="20" />
-                            Eliminar
-                        </Link>
-                    </Zoo>
-                ))
-            }
+            <Suspense fallback={
+                <>
+                    <div className='esqueleto-zoo' />
+                    <div className='esqueleto-zoo' />
+                    <div className='esqueleto-zoo' />
+                </>
+            }>
+                <Zoos />
+            </Suspense>
+           
         </div>
     )
 }
