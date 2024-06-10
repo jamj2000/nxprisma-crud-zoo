9318eeb89cb7c6a8c87703419566242170e2a78c
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-06-10 19:31:17 +0200

    add comment with select options alternative to input radio

--- src/components/ListaZoos.js
+++ src/components/ListaZoos.js
@@ -13,18 +13,29 @@ async function ListaZoos({ animalId, disabled }) {
     console.log(`ZOOLÓGICO del ANIMAL ${animalId}: `, zooId);
 
     return (
+        //// Tenemos 2 alternativas:
+        //// - usar inputs tipo radio
+        //// - usar options dentro de select
         <fieldset disabled={disabled}>
             <legend>Zoos</legend>
             {zoos?.map((zoo) => (
                 <div key={zoo.id}>
-                        {zooId == zoo.id
-                            ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
-                            : <input type='radio' name='zooId' value={zoo.id} />
-                        }
-                        {zoo.nombre}
+                    {zooId == zoo.id
+                        ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
+                        : <input type='radio' name='zooId' value={zoo.id} />
+                    }
+                    {zoo.nombre}
                 </div>
             ))}
         </fieldset>
+        //// Si quieres utilizar options dentro de un select, comenta las líneas anteriores y 
+        //// y descomenta las siguientes líneas 
+        // <select disabled={disabled} name="zooId">
+        //     {zoos?.map((zoo) => zoo.id == zooId
+        //         ? <option key={zoo.id} value={zoo.id} selected> {zoo.nombre} </option>
+        //         : <option key={zoo.id} value={zoo.id}> {zoo.nombre} </option>
+        //     )}
+        // </select>
     )
 }
 
