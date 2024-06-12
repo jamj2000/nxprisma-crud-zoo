e2d41d9ab2594379898144099cca075e704808dd
José A. Muñoz Jiménez <jamj2000@gmail.com>
2024-06-12 22:34:53 +0200

    select without disabled

--- src/components/ListaZoos.js
+++ src/components/ListaZoos.js
@@ -12,31 +12,46 @@ async function ListaZoos({ animalId, disabled }) {
 
     console.log(`ZOOLÓGICO del ANIMAL ${animalId}: `, zooId);
 
+    //// Tenemos 2 alternativas:
+    //// - usar inputs tipo radio
+    //// - usar options dentro de select
+
+
     return (
-        //// Tenemos 2 alternativas:
-        //// - usar inputs tipo radio
-        //// - usar options dentro de select
-        <fieldset disabled={disabled}>
-            <legend>Zoos</legend>
-            {zoos?.map((zoo) => (
-                <div key={zoo.id}>
-                    {zooId == zoo.id
-                        ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
-                        : <input type='radio' name='zooId' value={zoo.id} />
-                    }
-                    {zoo.nombre}
-                </div>
-            ))}
-        </fieldset>
-        //// Si quieres utilizar options dentro de un select, comenta las líneas anteriores y 
-        //// y descomenta las siguientes líneas 
-        // <select disabled={disabled} name="zooId">
-        //     {zoos?.map((zoo) => zoo.id == zooId
-        //         ? <option key={zoo.id} value={zoo.id} selected> {zoo.nombre} </option>
-        //         : <option key={zoo.id} value={zoo.id}> {zoo.nombre} </option>
-        //     )}
-        // </select>
+    <fieldset disabled={disabled}>
+        <legend>Zoos</legend>
+        {zoos?.map((zoo) => (
+            <div key={zoo.id}>
+                {zooId == zoo.id
+                    ? <input type='radio' name='zooId' value={zoo.id} defaultChecked />
+                    : <input type='radio' name='zooId' value={zoo.id} />
+                }
+                {zoo.nombre}
+            </div>
+        ))}
+    </fieldset>
     )
+
+    //// Si quieres utilizar options dentro de un select, comenta las líneas anteriores y 
+    //// y descomenta las siguientes líneas 
+    //// IMPORTANTE:  No poner disabled={disabled} en select puesto que no devuelve el valor correctamente
+
+    // if (disabled)
+    //     return (
+    //         <fieldset>
+    //             <input type='hidden' name="zooId" value={zooId} />
+    //             <legend>Zoo</legend>
+    //             <strong>{zoos.find(p => p.id == zooId).nombre}</strong>
+    //         </fieldset>
+    //     )
+    // else
+    //     return (
+    //         <select name="zooId" defaultValue={zooId} >
+    //             {zoos?.map((zoo) => <option key={zoo.id} value={zoo.id}> {zoo.nombre} </option>
+    //             )}
+    //         </select>
+    //     )
+
 }
 
 export default ListaZoos
